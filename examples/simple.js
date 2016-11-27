7220d2e0c24ecb9b8789efe975428d45208ac2e0
Kevin Jin <tost8295@gmail.com>
2016-11-28 00:16:06 +0900

    examples included

--- examples/simple.js
+++ examples/simple.js
@@ -13,7 +13,9 @@ function(err, article, meta) {
 article.title = 기사 제목
 article.content = 기사 내용
 
+examples :
 
+http://www.bbc.com/news/world-middle-east-38123829
 
  http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=104&oid=003&aid=0007614268
 
